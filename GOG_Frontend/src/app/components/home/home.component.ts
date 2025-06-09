b1dc84570c99b4b036b9d21a2415737dc712e9ce
gruiazu <gruiazu@g.educaand.es>
2025-06-09 16:56:04 +0200

    websockets+

--- GOG_Frontend/src/app/components/home/home.component.ts
+++ GOG_Frontend/src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { AuthService } from '../../services/authservice';
+import { WebsocketService } from '../../services/websocket.service';
 
 @Component({
   selector: 'app-home',
@@ -8,8 +9,60 @@ import { AuthService } from '../../services/authservice';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  matchFound: any = null;
+  waitingMessage: string = '';
   authService = inject(AuthService);
-  // Simulación de ELO
   userElo = 1200; 
+  
+
+  constructor(private websocketService: WebsocketService) { }
+
+  ngOnInit(): void {
+    // Verificar si el WebSocket está conectado
+    if (!this.websocketService.connected$.getValue()) {
+      console.log('WebSocket no está conectado. Reconectando...');
+      this.websocketService.connect();
+    }
+
+    // Suscribirse a los mensajes de matchmaking
+    this.websocketService.matchmakingMessage$.subscribe(message => {
+      if (message?.type === 'matchFound') {
+        if (!this.matchFound) {
+          this.matchFound = {};
+        }
+        this.matchFound.gameId = message.payload.gameId.toString();
+        this.waitingMessage = '';
+    
+        const currentUserId = Number(localStorage.getItem('UserId'));
+        console.log("Mi ID:", currentUserId);
+        console.log("Player1 ID:", this.matchFound.player1Id);
+        console.log("Player2 ID:", this.matchFound.player2Id);
+
+        if (currentUserId === message.payload.player1Id) {
+          localStorage.setItem('playerType', 'Player1');
+          localStorage.setItem('playerName', 'Jugador'+currentUserId);;
+        } else {
+          localStorage.setItem('playerType', 'Player2');
+          localStorage.setItem('playerName', 'Jugador'+currentUserId);
+        }
+    
+        console.log(`Partida encontrada. Eres ${localStorage.getItem('playerType')}`);
+      } else if (message?.type === 'waitingForMatch') {
+        this.waitingMessage = message.payload;
+      }
+    });
+    
+  }
+
+  buscarPartida(): void {
+    // Verificar nuevamente si el WebSocket está conectado antes de empezar el matchmaking
+    if (!this.websocketService.connected$.getValue()) {
+      console.log('WebSocket no está conectado. Reconectando...');
+      this.websocketService.connect();
+    }
+
+    // Empezar matchmaking
+    this.websocketService.requestMatchmaking();
+  }
 }

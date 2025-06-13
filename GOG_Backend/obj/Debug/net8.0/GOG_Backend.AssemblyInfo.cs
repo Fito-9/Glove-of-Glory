93f5744d098cc12346deaf6474af789ce62c833d
gruiazu <gruiazu@g.educaand.es>
2025-06-13 04:39:21 +0200

    he vuelto a atras

--- GOG_Backend/obj/Debug/net8.0/GOG_Backend.AssemblyInfo.cs
+++ GOG_Backend/obj/Debug/net8.0/GOG_Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GOG_Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d8d68f491e5e4c7c4c152e8159ade5167d17663")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3580b5a6638d07cbf284406f62f10f56fee5e12c")]
 [assembly: System.Reflection.AssemblyProductAttribute("GOG_Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GOG_Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

515ff68917c76ee9b57f4459615eb33ab4793331
gruiazu <gruiazu@g.educaand.es>
2025-06-13 23:52:54 +0200

    environment cambio

--- GOG_Frontend/src/environments/environment.development.ts
+++ GOG_Frontend/src/environments/environment.development.ts
@@ -1,6 +1,7 @@
 export const environment = {
-    apiUrl: 'https://localhost:7096/api/',
-    stripePublicKey: 'pk_test_51QFyCgKyA4zEE9fSA6xdwyhhJ4nxFSBkxWPaYEloMs9JkZnHXNoLlyafbiScul0uEGS8YnjOiqscoDYrOzJEHaHJ002mbOCSQ0',
-    socketUrl: 'wss://localhost:7096/ws'
+  apiUrl: "https://webserver.jarvisnetwork.es/api/",
+  stripePublicKey: 'pk_test_51QFyCgKyA4zEE9fSA6xdwyhhJ4nxFSBkxWPaYEloMs9JkZnHXNoLlyafbiScul0uEGS8YnjOiqscoDYrOzJEHaHJ002mbOCSQ0',
+  socketUrl: 'wss://webserver.jarvisnetwork.es/ws'
+
   };
   

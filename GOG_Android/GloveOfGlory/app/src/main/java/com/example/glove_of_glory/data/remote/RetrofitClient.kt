44316e6e29b7916c6b4b3c696610def80a14bbb6
Fito-9 <aburbel032@g.educaand.es>
2025-06-13 14:01:03 +0200

    Implementacion de dispositivo real

--- GOG_Android/GloveOfGlory/app/src/main/java/com/example/glove_of_glory/data/remote/RetrofitClient.kt
+++ GOG_Android/GloveOfGlory/app/src/main/java/com/example/glove_of_glory/data/remote/RetrofitClient.kt
@@ -1,6 +1,7 @@
 package com.example.glove_of_glory.data.remote
 
 import android.content.Context
+import android.os.Build
 import com.example.glove_of_glory.data.remote.interceptor.AuthInterceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -8,9 +9,27 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://10.0.2.2:5023/api/"
-    private var apiService: ApiService? = null
 
+    private const val REAL_DEVICE_IP = "192.168.1.144"
+    private const val EMULATOR_IP = "10.0.2.2"
+    private const val PORT = "5023"
+
+    private fun getBaseUrl(): String {
+
+        val isEmulator = Build.FINGERPRINT.startsWith("generic")
+                || Build.FINGERPRINT.startsWith("unknown")
+                || Build.MODEL.contains("google_sdk")
+                || Build.MODEL.contains("Emulator")
+                || Build.MODEL.contains("Android SDK built for x86")
+                || Build.MANUFACTURER.contains("Genymotion")
+                || (Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic"))
+                || "google_sdk" == Build.PRODUCT
+
+        val ip = if (isEmulator) EMULATOR_IP else REAL_DEVICE_IP
+        return "http://$ip:$PORT/api/"
+    }
+
+    private var apiService: ApiService? = null
 
     fun getInstance(context: Context): ApiService {
         if (apiService == null) {
@@ -18,14 +37,14 @@ object RetrofitClient {
                 level = HttpLoggingInterceptor.Level.BODY
             }
 
-
             val okHttpClient = OkHttpClient.Builder()
                 .addInterceptor(AuthInterceptor(context))
                 .addInterceptor(loggingInterceptor)
                 .build()
 
             val retrofit = Retrofit.Builder()
-                .baseUrl(BASE_URL)
+                // Usamos la función que elige la URL dinámicamente
+                .baseUrl(getBaseUrl())
                 .client(okHttpClient)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
